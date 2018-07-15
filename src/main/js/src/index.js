704c19765cee34c7a946f38c4851e334f269c5fd
seraco <sebastiaramon_1994@hotmail.com>
2018-07-15 19:08:52 +0100

    front end production build integrated for deployment

--- src/main/js/src/index.js
+++ src/main/js/src/index.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-dotenv.config();
+// dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
